0672cc871035c01e651143bed5e01b60fb261d8d
Xiao Fan <xiao.h.fan@gmail.com>
2017-02-12 09:13:58 -0800

    define extra assertions for commonly used custom assertions

--- test/test_helper/common_env.bash
+++ test/test_helper/common_env.bash
@@ -3,5 +3,7 @@
 load test_helper/bats-support/load
 load test_helper/bats-assert/load
 
+load test_helper/extra_assert
+
 HOME="$BATS_TEST_DIRNAME/mock_home"
 SRC_PATH="${SRC_PATH:-$BATS_TEST_DIRNAME/..}"
