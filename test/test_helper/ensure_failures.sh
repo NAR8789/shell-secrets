a68e3a3ee21337a3a94cabf5c642f101ce9c74fe
Xiao Fan <xiao.h.fan@gmail.com>
2017-02-12 10:00:55 -0800

    fail if a failure example does not trigger failures

--- test/test_helper/ensure_failures.sh
+++ test/test_helper/ensure_failures.sh
@@ -2,7 +2,13 @@
 
 # intended to be executed from base of repo
 
+FAILED_TO_FAIL=0
+
 for FAIL_SRC in test/failure_examples/*; do
   echo "$FAIL_SRC"
-  SRC_PATH="$FAIL_SRC" bats test
+  if SRC_PATH="$FAIL_SRC" bats test; then
+    FAILED_TO_FAIL=1
+  fi
 done
+
+exit $FAILED_TO_FAIL
