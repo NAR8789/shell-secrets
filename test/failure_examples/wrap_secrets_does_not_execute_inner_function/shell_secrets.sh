fe7b07c18640a99585f4b01c21de2adfc39e8c7e
Xiao Fan <xiao.h.fan@gmail.com>
2017-02-12 09:30:29 -0800

    failure case for ws not executing inner function

---
+++ test/failure_examples/wrap_secrets_does_not_execute_inner_function/shell_secrets.sh
@@ -0,0 +1,18 @@
+function load_secrets {
+  local SHELL_SECRETS="${SHELL_SECRETS:-$HOME/.shell_secrets.asc}"
+  if [ -r "$SHELL_SECRETS" ]; then
+    eval `gpg -d "$SHELL_SECRETS"`
+  fi
+}
+
+# I would much rather this be an alias. See https://github.com/NAR8789/shell-secrets/issues/12
+function los { load_secrets "$@"; }
+
+function wrap_secrets {
+  (
+    load_secrets
+  )
+}
+
+# I would much rather this be an alias. See https://github.com/NAR8789/shell-secrets/issues/12
+function ws { wrap_secrets "$@"; }
