cc301f1e174c14e602d3f529bbcfe565f467f29a
Xiao Fan <xiao.h.fan@gmail.com>
2017-02-24 21:51:05 -0800

    rename failure_examples to mutants

--- test/test_helper/ensure_failures.sh
+++ test/test_helper/ensure_failures.sh
@@ -10,14 +10,14 @@ OUTPUT_DIR="$ARTIFACT_DIR/fail_checks"
 rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
-for FAIL_SRC in test/failure_examples/*; do
-  FAIL_NAME="$(basename "$FAIL_SRC")"
-  SRC_PATH="$FAIL_SRC" bats test 2>"$OUTPUT_DIR/$FAIL_NAME.err.txt" >"$OUTPUT_DIR/$FAIL_NAME.out.txt"
+for MUTANT_SRC in test/mutants/*; do
+  MUTANT_NAME="$(basename "$MUTANT_SRC")"
+  SRC_PATH="$MUTANT_SRC" bats test 2>"$OUTPUT_DIR/$MUTANT_NAME.err.txt" >"$OUTPUT_DIR/$MUTANT_NAME.out.txt"
   if [ $? -eq 0 ] ; then
     FAILED_TO_FAIL=1
-    printf "\033[0;31mERROR: $FAIL_NAME did not cause a test failure\n"
+    printf "\033[0;31mERROR: $MUTANT_NAME did not cause a test failure\n"
   else
-    printf "\033[0;32m$FAIL_NAME fails tests, as expected\n"
+    printf "\033[0;32m$MUTANT_NAME fails tests, as expected\n"
   fi
 done
 
